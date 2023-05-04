4d97f7d1abfb087d743869e4d1fa59c86782a06a
kuma00 <kuma.gact@gmail.com>
2023-05-04 10:09:52 +0900

    auto commit

---
+++ src/messageCreate/su.ts
@@ -0,0 +1,14 @@
+import { MessageResponse } from "@src/types/index.js";
+import { reply, speak } from "@src/util/index.js";
+
+export const messageResponse: MessageResponse = {
+  name: "PI",
+  filter: (m) =>
+    ["いま"].some((i) => m.cleanContent == i) &&
+    !m.author.bot,
+  async execute(client, message) {
+    reply(message, "す。");
+    if (message.guild)
+      speak(client, message.guild, "す。", message.channelId);
+  },
+};
