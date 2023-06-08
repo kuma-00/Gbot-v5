e72e240962d86f639b88d79d55ea009883c1a432
kuma00 <kuma.gact@gmail.com>
2023-06-08 15:35:50 +0900

    auto commit

--- src/core/urlMetadata.ts
+++ src/core/urlMetadata.ts
@@ -7,8 +7,14 @@ export const urlReplace = async (text: string) => {
     text,
     /https?:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g,
     async (url: string) => {
-      const result = await unfurl(url);
-      return `URL:${result.open_graph.site_name ?? ""} ${result.title ?? ""}` ?? "";
+      try {
+        const result = await unfurl(url);
+        return (
+          `URL:${result.open_graph.site_name ?? ""} ${result.title ?? ""}` ?? ""
+        );
+      } catch {
+        return "URL省略";
+      }
     },
   );
 };
