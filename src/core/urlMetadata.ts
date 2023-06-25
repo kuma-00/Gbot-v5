a6b956d50b2a7723ec805fc8ed5aa19dcc244c83
kuma00 <kuma.gact@gmail.com>
2023-06-25 16:15:37 +0900

    auto commit

--- src/core/urlMetadata.ts
+++ src/core/urlMetadata.ts
@@ -10,7 +10,7 @@ export const urlReplace = async (text: string) => {
       try {
         const result = await unfurl(url);
         return (
-          `URL:${result.open_graph.site_name ?? ""} ${result.title ?? ""}` ?? ""
+          `URL:${result.open_graph.site_name ?? ""} ${result.title?.replace(/https?:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g,"") ?? ""}` ?? ""
         );
       } catch {
         return "URL省略";
