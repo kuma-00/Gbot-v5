d9a29abce5253c7f03374a817b4d5bee5542c4c5
BuildTools <unconfigured@null.spigotmc.org>
2023-08-03 03:36:47 +0900

    auto commit

--- src/games/r2i3.ts
+++ src/games/r2i3.ts
@@ -10,6 +10,7 @@ import { default as Kuroshiro} from "kuroshiro/lib/index.js";
 const kuroshiro = new Kuroshiro.default();
 import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
 await kuroshiro.init(new KuromojiAnalyzer());
+import split from "graphemesplit";
 
 function kanaToHira(str :string) {
   return str.replace(/[\u30a1-\u30f6]/g, function(match) {
@@ -31,7 +32,7 @@ export const minigame: MinigameConstructor = class r2i3 extends MinigameBase {
   };
   data: MinigameData;
   client: ExtensionClient;
-  history: string[] = ["しりとり"];
+  history: string[] = [["し","り","と","り"]];
   index = 0;
   collector!: MessageCollector;
   constructor(client: ExtensionClient, data: MinigameData) {
@@ -53,13 +54,13 @@ export const minigame: MinigameConstructor = class r2i3 extends MinigameBase {
   }
   async collect(m: Message): Promise<void> {
     const text = m.cleanContent;
-    const hiragana = await kuroshiro.convert(kanaToHira(text), { to: "hiragana" });
+    const hiragana = split(await kuroshiro.convert(kanaToHira(text), { to: "hiragana" }));
     const last = this.history[this.history.length - 1];
     const isChain = last.at(-1) == hiragana.at(0);
-    const isNotRepeat = !this.history.includes(hiragana);
+    const isNotRepeat = !this.history.includes(hiragana.join(""));
     const isAuthor = m.author.id == this.nowMember.id;
     if (isChain && isNotRepeat && isAuthor) {
-      this.history.push(hiragana);
+      this.history.push(hiragana.join(""));
       m.react("☑");
       this.next();
       console.log(`${hiragana}の｢${hiragana.at(-1)}(${hiragana.at(-1)?.charCodeAt(0).toString(16)})｣`);
