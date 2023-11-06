59cf58dbcfed32aa07a1c3117493ac9eb6f3b4da
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 17:22:11 +0900

    auto commit

--- src/messageCreate/twitter.ts
+++ src/messageCreate/twitter.ts
@@ -1,16 +1,16 @@
-import { twitterEmbed } from "@src/core/urlMetadata.js";
+// import { twitterEmbed } from "@src/core/urlMetadata.js";
 import { MessageResponse } from "@src/types/index.js";
 
 export const messageResponse: MessageResponse = {
   name: "twitter",
-  filter: (m) => m.channel.id == "1124762255741956177" || m.channel.id == "886981303138910232",
+  filter: (m) => m.channel.id == "1124762255741956177",
   async execute(client, message) {
     const url = message.cleanContent.match(/https?:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/)?.[0] ?? "";
     let m = message;
     if(url.match(/twitter.com|x.com|t.co/)){
-      const embeds = await twitterEmbed(new URL(url));
-      m = await message.reply({embeds,allowedMentions:{parse:[]}});
-      // m = await message.reply({content:url.replace(/twitter.com|x.com/,"vxtwitter.com"),allowedMentions:{parse:[]}});
+      // const embeds = await twitterEmbed(new URL(url));
+      // m = await message.reply({embeds,allowedMentions:{parse:[]}});
+      m = await message.reply({content:url.replace(/twitter.com|x.com/,"vxtwitter.com"),allowedMentions:{parse:[]}});
     }
     m.react("❤");
     m.react("🔁");
