fa3e5fe6e6cff91240370fc98f40513b5a14a813
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 17:05:26 +0900

    auto commit

--- src/messageCreate/twitter.ts
+++ src/messageCreate/twitter.ts
@@ -5,13 +5,14 @@ export const messageResponse: MessageResponse = {
   name: "twitter",
   filter: (m) => m.channel.id == "1124762255741956177" || m.channel.id == "886981303138910232",
   async execute(client, message) {
-    message.react("❤");
-    message.react("🔁");
     const url = message.cleanContent.match(/https?:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/)?.[0] ?? "";
+    let m = message;
     if(url.match(/twitter.com|x.com|t.co/)){
       // const embeds = await twitterEmbed(new URL(url));
-      message.reply(url.replace(/twitter.com|x.com/,"vxtwitter.com"));
+      m = await message.reply({content:url.replace(/twitter.com|x.com/,"vxtwitter.com"),allowedMentions:{parse:[]}});
     }
+    m.react("❤");
+    m.react("🔁");
 
   },
 };
